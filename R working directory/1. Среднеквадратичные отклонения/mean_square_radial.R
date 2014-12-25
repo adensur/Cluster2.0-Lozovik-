f445f7043a410727a86da33d08a4b3de4038a0f3
Maksim <adensur@gmail.com>
2014-12-25 10:44:58 +0300

    Calculated temperature dependencies

--- R working directory/1. Среднеквадратичные отклонения/mean_square_radial.R
+++ R working directory/1. Среднеквадратичные отклонения/mean_square_radial.R
@@ -1,5 +1,8 @@
 ##mean squared radial deviation
+
+##initialization
 N=27         #number of particles
+data=data.frame()
 velocity=function(aggr){   #function calculates velocity distribution over all the array. 
         M=dim(aggr)[3]
         N=dim(aggr)[2]
@@ -19,23 +22,30 @@ aggr.rad=function(aggr){
         }
         radial
 }
-t=c(0.0001,0.001,0.01,0.1,0.2)
 
-data=matrix(nrow=0,ncol=2)
-colnames(data)=c("temperature","sd")
+##setting the desired standard deviation points, in which we calculate sd(rad)
+t=seq(0.001,0.2,by=0.003)
+K1=500
+K2=15
+dt=0.05
 #
+
+##calculate!
 for(i in 1:length(t)){
-r=reinit(27) #initial equilibrium state, T=0
-r=temp(r, temperature = t[i])
-arr=molecular2(r,1000,50,dt=0.01)
-rad1=aggr.rad(arr)
-hist(rad1,50)
+r=reinit(N) #initial equilibrium state, T=0
+r=temp(r, temperature = t[i])    ##setting temperature
+arr=molecular2(r,K1,K2,dt=dt)    ##calculating an array of coordinates/velocities
+rad1=aggr.rad(arr)               ##calculating mean radius
 sd1=NULL
 for(n in 1:N){
         sd1=c(sd1,sd(rad1[n,]))
 }
 #
 sd10=mean(sd1)
-vector=c(Temp,mean(sd1))
+vector=c(t[i],Temp,mean(sd1))
 data=rbind(data,vector)
+#print()
 }
+colnames(data)=c("sd","temp","rad")
+
+
